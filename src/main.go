42ebd4878d5ca06ba39f53a1b7a1f72bc94f5976
piotsik <36536181+piotsik@users.noreply.github.com>
2020-02-13 21:16:29 +0100

    Added win verification

--- src/main.go
+++ src/main.go
@@ -30,6 +30,7 @@ type Game struct {
 	mousePos rl.Vector2
 	board    [boardSize][boardSize]Tile
 	turn     string
+	winner   string
 }
 
 func main() {
@@ -57,6 +58,7 @@ func (g *Game) Init() {
 	g.gameOver = false
 	g.turn = "o"
 
+	// Prepare the board specific parameters
 	for i := 0; i < boardSize; i++ {
 		for j := 0; j < boardSize; j++ {
 			g.board[i][j].position.Width = tileSize
@@ -72,18 +74,17 @@ func (g *Game) Init() {
 // Update method
 func (g *Game) Update() {
 	g.gameOver = false
-
 	g.mousePos = rl.GetMousePosition()
 
-	// Check if mouse is inside a tile
-	if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
-		fmt.Println("click")
+	// Check on where mouse was pressed
+	if rl.IsMouseButtonPressed(rl.MouseLeftButton) && g.winner == "" {
 		for i := 0; i < boardSize; i++ {
 			for j := 0; j < boardSize; j++ {
 				if rl.CheckCollisionPointRec(g.mousePos, g.board[i][j].position) {
 					if g.board[i][j].state == "" {
-						fmt.Println(g.board[i][j].index)
 						g.board[i][j].state = g.turn
+						CheckWin(i, j, g)
+						// Change turn
 						if g.turn == "x" {
 							g.turn = "o"
 						} else {
@@ -101,6 +102,7 @@ func (g *Game) Draw() {
 	rl.BeginDrawing()
 	rl.ClearBackground(rl.White)
 
+	// Draw the board
 	for i := 0; i < boardSize; i++ {
 		for j := 0; j < boardSize; j++ {
 			rl.DrawRectangleLinesEx(g.board[i][j].position, shapeThickness, rl.Black)
@@ -108,8 +110,65 @@ func (g *Game) Draw() {
 		}
 	}
 
-	//Draw mouse position
-	rl.DrawText(fmt.Sprintf("x=%4.0f y=%4.0f turn=%s", rl.GetMousePosition().X, rl.GetMousePosition().Y, g.turn), 0, 0+screenSize, 20, rl.Black)
+	// Draw menu items
+	rl.DrawText(fmt.Sprintf("Turn=%s", g.turn), 0+10, 0+screenSize, 20, rl.Black)
+	rl.DrawText(fmt.Sprintf("Winner=%s", g.winner), screenSize/2+10, 0+screenSize, 20, rl.Black)
+
+	// Draw game won screen
+	if g.winner != "" {
+		rl.DrawRectangle(0, 0, screenSize, screenSize+menuHeight, rl.Black)
+		rl.DrawText(fmt.Sprintf("Game over \n  '%s' won!", g.winner), 40, 120, 50, rl.White)
+		// rl.EndDrawing()
+	}
 
 	rl.EndDrawing()
+}
+
+// CheckWin checks if there is a winner
+func CheckWin(i int, j int, g *Game) {
+	var winner bool = true
+	// Check row, column and diagonals
+	for x := 0; x < boardSize; x++ {
+		if g.board[i][x].state != g.turn {
+			winner = false
+			break
+		}
+	}
+	if winner {
+		g.winner = g.turn
+		g.gameOver = true
+	}
+	winner = true
+	for x := 0; x < boardSize; x++ {
+		if g.board[x][j].state != g.turn {
+			winner = false
+			break
+		}
+	}
+	if winner {
+		g.winner = g.turn
+		g.gameOver = true
+	}
+	winner = true
+	for x := 0; x < boardSize; x++ {
+		if g.board[x][x].state != g.turn {
+			winner = false
+			break
+		}
+	}
+	if winner {
+		g.winner = g.turn
+		g.gameOver = true
+	}
+	winner = true
+	for x := 0; x < boardSize; x++ {
+		if g.board[x][boardSize-x-1].state != g.turn {
+			winner = false
+			break
+		}
+	}
+	if winner {
+		g.winner = g.turn
+		g.gameOver = true
+	}
 }
